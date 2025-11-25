dae9e9577e83de202ce1badb4e820da92ccfbd0a
Ajay Sain <ajaysain00786@gmail.com>
2025-11-25 22:48:21 +0530

    moving index.html

--- forge.config.js
+++ forge.config.js
@@ -37,7 +37,7 @@ module.exports = {
           config: './webpack.renderer.config.js',
           entryPoints: [
             {
-              html: './src/index.html',
+              html: './src/public/index.html',
               js: './src/renderer',
               name: 'main_window',
               preload: {
